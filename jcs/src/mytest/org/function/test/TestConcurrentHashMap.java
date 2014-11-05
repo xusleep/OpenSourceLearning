53c9c1199fd2e45c3e29b12e3c3a1f514704480d
zhonxu <xusleep@gmail.com>
2014-11-05 15:33:58 +0800

    move open source to the same respository

---
+++ jcs/src/mytest/org/function/test/TestConcurrentHashMap.java
@@ -0,0 +1,15 @@
+package org.function.test;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class TestConcurrentHashMap {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ConcurrentHashMap chm = new ConcurrentHashMap();
+	}
+
+}
